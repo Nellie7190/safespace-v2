7855caeea320236daf4c49495ed70ec1f5117e5b
Your Name <nellie7190@gmail.com>
2026-01-27 16:18:32 -0500

    dummy data added for use

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('SafeSpace v2 loaded');
+const locations = [
+    {name: "One", petFriendly: true, lgbtOwned: false, blackOwned: true, womanOwned: true, estType: 'healthcare'},
+    {name: "Two", petFriendly: true, lgbtOwned: true, blackOwned: false, womanOwned: false, estType: 'nightlife'},
+    {name: "Three", petFriendly: false, lgbtOwned: false, blackOwned: true, womanOwned: true, estType: 'foodservice'},
+    {name: "Four", petFriendly: false, lgbtOwned: true, blackOwned: false, womanOwned: true, estType: 'education'},
+    {name: "Five", petFriendly: true, lgbtOwned: false, blackOwned: false, womanOwned: false, estType: 'retail'}
+]
+// console.log(locations.filter(l => l.estType == 'retail'));
