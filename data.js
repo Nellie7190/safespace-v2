081a253c1bef5081a596d69f16db52db67b2773b
Your Name <nellie7190@gmail.com>
2026-02-06 19:31:31 -0500

    added data to separate file

---
+++ data.js
@@ -0,0 +1,9 @@
+const locations = [
+    {name: "One", petFriendly: true, lgbtqOwned: false, blackOwned: true, womanOwned: true, estType: 'healthcare'},
+    {name: "Two", petFriendly: true, lgbtqOwned: true, blackOwned: false, womanOwned: false, estType: 'nightlife'},
+    {name: "Three", petFriendly: false, lgbtqOwned: false, blackOwned: true, womanOwned: true, estType: 'foodservice'},
+    {name: "Four", petFriendly: false, lgbtqOwned: true, blackOwned: false, womanOwned: true, estType: 'education'},
+    {name: "Five", petFriendly: true, lgbtqOwned: false, blackOwned: false, womanOwned: false, estType: 'retail'}
+];
+
+export { locations };
